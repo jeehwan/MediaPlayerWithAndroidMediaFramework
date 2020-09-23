1e3ca3d6f51f4396e2ad642f739c84f312d5abca
Jeehwan Park <jeehwan.park@navercorp.com>
2020-09-23 23:19:42 +0900

    complete tutorial2

--- app/src/main/java/com/navercorp/deview/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/navercorp/deview/mediaplayer/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.navercorp.deview.mediaplayer
 
 import android.graphics.SurfaceTexture
-import android.media.MediaCodec
-import android.media.MediaExtractor
-import android.media.MediaFormat
+import android.media.*
 import android.os.Bundle
 import android.view.Surface
 import android.view.TextureView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
 
 // http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
 private val MEDIA_FILE = "bbb_sunflower_1080p_30fps_normal.mp4"
 private val TIMEOUT_US = 10_000L
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextureView.SurfaceTextureListener {
 
     private lateinit var view: AutoFitTextureView
+    private lateinit var videoThread: VideoDecodeThread
+    private lateinit var audioThread: AudioDecodeThread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,35 +24,52 @@ class MainActivity : AppCompatActivity() {
 
         view = textureView
         view.setAspectRatio(16, 9)
-        view.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-            override fun onSurfaceTextureSizeChanged(
-                surface: SurfaceTexture,
-                width: Int,
-                height: Int
-            ) = Unit
+        view.surfaceTextureListener = this
 
-            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) = Unit
+        val afd = assets.openFd(MEDIA_FILE)
+        val extractor = MediaExtractor().apply { setDataSource(afd) }
 
-            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean = true
+        val videoTrackIndex = extractor.firstVideoTrack
+            ?: error("This media file doesn't contain any video tracks")
+        val videoExtractor = MediaExtractor().apply { setDataSource(afd) }
 
-            override fun onSurfaceTextureAvailable(
-                surface: SurfaceTexture,
-                width: Int,
-                height: Int
-            ) {
-                createVideoThread(Surface(surface))
-            }
-        }
+        val audioTrackIndex = extractor.firstAudioTrack
+            ?: error("This media file doesn't contain any audio tracks")
+        val audioExtractor = MediaExtractor().apply { setDataSource(afd) }
+
+        afd.close()
+        extractor.release()
+
+        audioThread = AudioDecodeThread(audioExtractor, audioTrackIndex)
+        videoThread = VideoDecodeThread(videoExtractor, videoTrackIndex)
     }
 
-    private fun createVideoThread(surface: Surface) = thread {
-        val extractor = assets.openFd(MEDIA_FILE).use {
-            MediaExtractor().apply { setDataSource(it) }
-        }
+    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) = Unit
 
-        val trackIndex = extractor.firstVideoTrack
-            ?: error("This media file doesn't contain any video tracks")
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture) = Unit
 
+    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean = true
+
+    override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+        videoThread.setSurface(Surface(surface))
+        videoThread.start()
+        audioThread.start()
+    }
+
+}
+
+class VideoDecodeThread(
+    private val extractor: MediaExtractor,
+    private val trackIndex: Int
+) : Thread() {
+
+    private lateinit var surface: Surface
+
+    fun setSurface(surface: Surface) {
+        this.surface = surface
+    }
+
+    override fun run() {
         extractor.selectTrack(trackIndex)
 
         val format = extractor.getTrackFormat(trackIndex)
@@ -106,6 +122,100 @@ class MainActivity : AppCompatActivity() {
                             outEos = true
                         } else {
                             decoder.releaseOutputBuffer(outputIndex, true)
+                        }
+                    }
+                    MediaCodec.INFO_TRY_AGAIN_LATER -> Unit
+                    MediaCodec.INFO_OUTPUT_FORMAT_CHANGED -> Unit
+                    MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED -> Unit
+                    else -> error("unexpected result from decoder.dequeueOutputBuffer: $outputIndex")
+                }
+            }
+        }
+    }
+}
+
+class AudioDecodeThread(
+    private val extractor: MediaExtractor,
+    private val trackIndex: Int
+) : Thread() {
+
+    override fun run() {
+        extractor.selectTrack(trackIndex)
+
+        val format = extractor.getTrackFormat(trackIndex)
+        val mime = format.getString(MediaFormat.KEY_MIME)
+            ?: error("Audio track must have the mime type")
+        val sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE)
+        val channels = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT)
+        val channelMask = if (channels == 1) AudioFormat.CHANNEL_OUT_MONO else AudioFormat.CHANNEL_OUT_STEREO
+
+        val decoder = MediaCodec.createDecoderByType(mime).apply {
+            configure(format, null, null, 0)
+            start()
+        }
+
+        val audioTrack = AudioTrack.Builder()
+            .setAudioAttributes(AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build())
+            .setAudioFormat(AudioFormat.Builder()
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setSampleRate(sampleRate)
+                .setChannelMask(channelMask)
+                .build())
+            .setTransferMode(AudioTrack.MODE_STREAM)
+            .setBufferSizeInBytes(1024 * 100)
+            .build()
+
+        audioTrack.play()
+
+        doExtract(extractor, decoder, audioTrack)
+
+        decoder.stop()
+        decoder.release()
+        extractor.release()
+        audioTrack.stop()
+        audioTrack.release()
+    }
+
+    private fun doExtract(extractor: MediaExtractor, decoder: MediaCodec, audioTrack: AudioTrack) {
+        val info = MediaCodec.BufferInfo()
+
+        var inEos = false
+        var outEos = false
+
+        while (!outEos) {
+            if (!inEos) {
+                when (val inputIndex = decoder.dequeueInputBuffer(TIMEOUT_US)) {
+                    in 0..Int.MAX_VALUE -> {
+                        val inputBuffer = decoder.getInputBuffer(inputIndex)!!
+                        val chunkSize = extractor.readSampleData(inputBuffer, 0)
+                        if (chunkSize < 0) {
+                            decoder.queueInputBuffer(inputIndex, 0, 0, -1,
+                                MediaCodec.BUFFER_FLAG_END_OF_STREAM)
+                            inEos = true
+                        } else {
+                            val sampleTimeUs = extractor.sampleTime
+                            decoder.queueInputBuffer(inputIndex, 0, chunkSize, sampleTimeUs, 0)
+                            extractor.advance()
+                        }
+                    }
+                    else -> Unit
+                }
+            }
+
+            if (!outEos) {
+                when (val outputIndex = decoder.dequeueOutputBuffer(info, TIMEOUT_US)) {
+                    in 0..Int.MAX_VALUE -> {
+                        if ((info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                            decoder.releaseOutputBuffer(outputIndex, false)
+                            outEos = true
+                        } else {
+                            val outputBuffer = decoder.getOutputBuffer(outputIndex)!!
+                            outputBuffer.position(info.offset)
+                            outputBuffer.limit(info.offset + info.size)
+                            audioTrack.write(outputBuffer, info.size, AudioTrack.WRITE_BLOCKING)
+                            decoder.releaseOutputBuffer(outputIndex, false)
                         }
                     }
                     MediaCodec.INFO_TRY_AGAIN_LATER -> Unit
